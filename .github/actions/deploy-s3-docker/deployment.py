de20aaacd3ae9e4e8acf514c439d86ce614c8613
Samer Osman <s.osman@reply.de>
2024-03-20 11:24:24 +0100

    fix action

--- .github/actions/deploy-s3-docker/deployment.py
+++ .github/actions/deploy-s3-docker/deployment.py
@@ -1,12 +1,10 @@
 import os
-import boto3
-from botocore.config import Config
 
 ## This code is just a test to simulate how to create an action with docker container
 def run():
     bucket = os.environ['INPUT_BUCKET']
     
-    print('bucket test: ' + bucket)
+    print('bucket test name: ' + bucket)
 
 
 if __name__ == '__main__':
